331d9d42d97a8e82f933362506245ec70632f830
이현호 <abcd1e34@gmail.com>
2026-01-23 11:12:52 +0900

    feat : gsap

--- src/components/common/inner/CommonInner.jsx
+++ src/components/common/inner/CommonInner.jsx
@@ -42,6 +42,10 @@ export const CommonInner = () => {
         const lenis = getLenis();
         window.history.scrollRestoration = 'manual';
         lenis.scrollTo(0, { immediate: true, duration: 0 });
+
+        requestAnimationFrame(() => {
+            ScrollTrigger.refresh();
+        });
     },[])
 
     return (
